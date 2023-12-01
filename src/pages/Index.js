0bafd89778f086e831179eb75d37dee53cba936b
songganam <dev.ganam@gmail.com>
2023-12-01 18:03:30 +0900

    feat : 기본 라우터 진행

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,16 +2,28 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
-import { redirect } from "react-router";
+import { Link, useNavigate } from "react-router-dom";
 
 const Index = () => {
+  // Link 태그 아닌 방식으로 path 전달 및 이동
+  const navigate = useNavigate();
+  // 기능을 정의하겠다.
+  const handleClickMenu = () => {
+    // 이전 path 로 가기
+    navigate("/home");
+  };
   return (
     <>
-      <Header>My Diary</Header>
+      <Header handleClickMenu={handleClickMenu} icon="bt_home.svg">
+        My Diary
+      </Header>
       <Main>
         <h2 style={{ textAlign: "center", color: "red" }}>웹서비스 첫화면</h2>
+        <Link to="/diary">다이어리 가기</Link>
       </Main>
-      <Footer></Footer>
+      <Footer>
+        <p>Copyright 2023. 첫화면</p>
+      </Footer>
     </>
   );
 };
--- src/pages/diary/DiaryAdd.js
+++ src/pages/diary/DiaryAdd.js
@@ -2,11 +2,18 @@ import React from "react";
 import Header from "../../components/Header";
 import Main from "../../components/Main";
 import Footer from "../../components/Footer";
+import { Link, useNavigate } from "react-router-dom";
 
 const DiaryAdd = () => {
+  const navigate = useNavigate();
+  const handleClickMenu = () => {
+    navigate(-1);
+  };
   return (
     <>
-      <Header>DiaryAdd</Header>
+      <Header handleClickMenu={handleClickMenu} icon="bt_back.svg">
+        DiaryAdd
+      </Header>
       <Main>
         <h2 style={{ textAlign: "center", color: "red" }}>
           다이어리 추가 화면
